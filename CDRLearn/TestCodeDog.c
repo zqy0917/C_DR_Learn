3ce2c50e966c65d9809faec9fdcc3804909ba88c
alexqyzhao <alexqyzhao@tencent.com>
2020-05-06 21:42:28 +0800

    test

--- CDRLearn/TestCodeDog.c
+++ CDRLearn/TestCodeDog.c
@@ -144,7 +144,52 @@ void init(){
     
 }
 
+void test11112132322(){
+    
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    
+}
+
 void test111121323(){
     
     printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaa");
+    printf("asaaawqeqwewqewqewqewqewqewqewqewqewqewqewqewqewqerwrewfdsfsdfewfewfewfewfewfewfewfewfewfewfewfewfewfeguwfgewfugewugfugeiugweugifugeiuwgfiugueiwgfiuguiewgfuigweuifguiwgeuifguiwegfuiguiewgfuigewuifguiweguifguiewguifguiewgfuigweuifguiewguifguwiefguiweguewgfuigweui");
 }
